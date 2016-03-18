544a533293a1e1aad1ad59ba584a8a2e3a321679
Karl <KDirzys@student.naperville203.org>
2016-03-18 09:28:06 -0500

    Colour Change

--- FractalTreeLab/FractalTreePanel.java
+++ FractalTreeLab/FractalTreePanel.java
@@ -4,7 +4,6 @@ public class FractalTreePanel extends JPanel
 {
     private final int PANEL_WIDTH = 1000;
     private final int PANEL_HEIGHT = 1000;
-    private final double BRANCH_DEC = 0.80;
     private final double ANGLE = Math.sqrt(3.0) / 6;
     private int current;
     /**
@@ -28,14 +27,18 @@ public class FractalTreePanel extends JPanel
             int x2 = x1 - (int) (Math.sin(Math.toRadians(angle)) * depth * 10.0);
             int y2 = y1 - (int) (Math.cos(Math.toRadians(angle)) * depth * 10.0);
             g.drawLine(x1, y1, x2, y2);
-            drawFractal(g, x2, y2, angle - 20, depth - 1);
-            drawFractal(g, x2, y2, angle + 20, depth - 1);
-            if (depth >= 8)
+            //drawFractal(g, x2, y2, angle - 20, depth - 1);
+            //drawFractal(g, x2, y2, angle + 20, depth - 1);
+            if (depth >= 2)
             {
-             g.setColor(Color.BLUE);
+             g.setColor(Color.RED);
             }
             else
-            g.setColor(Color.GREEN);
+            {
+             g.setColor(Color.GREEN);
+            }
+            drawFractal(g, x2, y2, angle - 20, depth - 1);
+            drawFractal(g, x2, y2, angle + 20, depth - 1);
         }
     }
 
