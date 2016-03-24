50490d7dac76a1750347819e8240bba0c0aa390a
Karl <KDirzys@student.naperville203.org>
2016-03-24 10:28:31 -0500

    dun with word count

--- Practice/WordChoice.java
+++ Practice/WordChoice.java
@@ -11,27 +11,50 @@ import java.io.*;
  */
 public class WordChoice
 {
+    Scanner in;
     public WordChoice(String fileName)
     {
         int counter = 0;
 
         File read = new File(fileName);
-
         try
         {
-            Scanner in = new Scanner(read);
+            in = new Scanner(read);
+            //File read = new File(fileName);
         }
-        catch
+        catch (IOException exception)
         {
-
+          System.out.println("file not found");
         }
-        Scanner in = new Scanner(read);
+        //Scanner in = new Scanner(read);
+        
         in.useDelimiter(" ");
         while(in.hasNext())
         {
             String word = in.next();
+            counter += 1;
         }
         System.out.println("Number of words: " + counter);
+        counter = 0;
+        
+        in.useDelimiter("/n ");
+       
+        while(in.hasNext())
+        {
+            char ch = in.next().charAt(0);
+            counter += 1;
+        }
+        System.out.println("Number of characters: " + counter);
+        counter = 0;
+        
+        in.useDelimiter(" ");
+        
+        while(in.hasNext())
+        {
+            String line = in.next();
+            counter += 1;
+        }
+        System.out.println("Number of lines: " + counter);
     }
 
     public static void main (String[] args)
